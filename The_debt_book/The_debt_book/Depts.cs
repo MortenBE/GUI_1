8e9a83ac86029bd82e670c2e4b7d964942bf56ec
GustavSorensen <54596202+GustavSorensen@users.noreply.github.com>
2020-03-15 13:50:55 +0100

    Det vi lavede lørdag

---
+++ The_debt_book/The_debt_book/Depts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_debt_book
+{
+    public class Depts 
+    {
+        string _name;
+        int _value;
+        string _combined;
+
+        public Depts()
+        { }
+        public Depts(string Name, int Value)
+        {
+            _name = Name;
+            _value = Value;
+            _combined = Name + "                " + Value;
+        }
+
+        public string Combined
+        {
+            get
+            {
+                return _combined;
+            }
+        }
+        public string Name 
+        {
+            get
+            { 
+                return _name;
+            }
+            set
+            { 
+                _name = Name;
+            }
+        }
+
+        public int Value
+        {
+            get
+            { 
+                return _value;
+            }
+            set
+            {
+                _value = Value;
+            }
+        }
+    }
+}
---
+++ The_debt_book/The_debt_book/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using The_debt_book.Views;
+
+namespace The_debt_book
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        ObservableCollection<Depts> deptslist;
+
+        public MainWindowViewModel()
+        {
+            deptslist = new ObservableCollection<Depts>
+            {
+                new Depts("Gustav", 100),
+                new Depts("Morten", 200)
+            };
+        }
+
+        public ObservableCollection<Depts> Deptslist
+        {
+            get { return deptslist; }
+            set { SetProperty(ref deptslist, value); }
+        }
+        ICommand _newCommand;
+        public ICommand AddNewDeptCommand
+        {
+            get
+            {
+                return _newCommand ?? (_newCommand = new DelegateCommand(() =>
+                {
+                    Deptslist.Add(new Depts());
+                }));
+            }
+        }
+
+        private ICommand _addClickCommand;
+
+        public ICommand ButtonAddClickCommand
+        {
+            get
+            {
+                return _addClickCommand ?? (_addClickCommand = new DelegateCommand(() =>
+                { 
+                    AddDeptorView newDept = new AddDeptorView();
+                    newDept.Show();
+                }));
+            }
+
+        }
+
+        private ICommand _closeCommand;
+
+        public ICommand ButtonCloseCommand
+        {
+            get
+            {
+                return _addClickCommand ?? (_addClickCommand = new DelegateCommand(() =>
+                {
+
+                }));
+            }
+        }
+    }
+}
