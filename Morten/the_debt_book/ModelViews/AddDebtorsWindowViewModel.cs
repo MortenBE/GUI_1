a30ac068f58f208b0c2fab4127da3c8a9889e119
MortenBE <Ehlersmorten@gmail.com>
2020-03-16 20:57:54 +0100

    ny forsøg
    andet forsøg

---
+++ Morten/the_debt_book/ModelViews/AddDebtorsWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+//using Debt_Book.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace the_debt_book.ModelViews
+{
+    class AddDebtorsWindowViewModel : BindableBase
+    {
+        public AddDebtorsWindowViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ The_debt_book/ModelViews/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+//using the_debt_book.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace the_debt_book.ModelViews
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        #region Commands
+        private ICommand _buttonAddClickCommand;
+
+        public ICommand ButtonAddClickCommand
+        {
+            get
+            {
+                return _buttonAddClickCommand ?? (_buttonAddClickCommand = new DelegateCommand(() =>
+                {
+                    Views.Add_Debtors newDept = new Views.Add_Debtors();
+                    newDept.Show();
+                }));
+            }
+
+        }
+        #endregion
+    }
+}
