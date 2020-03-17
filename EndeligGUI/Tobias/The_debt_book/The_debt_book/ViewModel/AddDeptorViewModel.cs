a374c954a79198e43a2082a10422255cb0468ffc
MortenBE <Ehlersmorten@gmail.com>
2020-03-17 09:34:43 +0100

    make it pretty

---
+++ EndeligGUI/Tobias/The_debt_book/The_debt_book/ViewModel/AddDeptorViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using TheDebtBook.Models;
+
+namespace TheDebtBook
+{
+    public class AddDebtorViewModel : BindableBase
+    {
+        private Depts _deptor;
+
+        public AddDebtorViewModel(Depts deptor)
+        {
+            Deptor = deptor;
+        }
+
+        public Depts Deptor
+        {
+            get => _deptor;
+            set => SetProperty(ref _deptor, value);
+        }
+
+        private ICommand _okButtonCommand;
+
+        public ICommand OkButtonCommand
+        {
+            get => _okButtonCommand ?? (_okButtonCommand =
+                       new DelegateCommand(OkButtonCommandExecute, OkButtonCommandCanExecute)
+                           .ObservesProperty(() => Deptor.Name).ObservesProperty(() => Deptor.Debt));
+        }
+
+        private void OkButtonCommandExecute()
+        { }
+
+        private bool OkButtonCommandCanExecute()
+        {
+            return CheckEmpty;
+        }
+
+        public bool CheckEmpty
+        {
+            get
+            {
+                bool isValid = true;
+                if (string.IsNullOrWhiteSpace(Deptor.Name))
+                    isValid = false;
+                if (string.IsNullOrWhiteSpace(Deptor.Debt.ToString()))
+                    isValid = false;
+                return isValid;
+            }
+        }
+    }
+}
---
+++ EndeligGUI/Tobias/The_debt_book/The_debt_book/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using TheDebtBook.Models;
+using The_debt_book.Views;
+
+namespace TheDebtBook
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private int _index;
+        private Depts _currentDeptor;
+        private ObservableCollection<Depts> _deptors;
+        
+
+        public MainWindowViewModel()
+        {
+            Deptors = new ObservableCollection<Depts>()
+            {
+                new Depts("Gustav", 100),
+                new Depts("Morten", 150),
+            };
+        }
+
+        public ObservableCollection<Depts> Deptors
+        {
+            get => _deptors;
+            set => SetProperty(ref _deptors, value);
+        }
+
+        public int Index
+        {
+            get => _index;
+            set => SetProperty(ref _index, value);
+        }
+
+        public Depts CurrentDeptor
+        {
+            get => _currentDeptor;
+            set => SetProperty(ref _currentDeptor, value);
+        }
+
+        private ICommand _addDebtorCommand;
+        
+        public ICommand AddDebtorCommand
+        {
+            get => _addDebtorCommand ?? (_addDebtorCommand = new DelegateCommand(() =>
+            {
+                var newDeptor = new Depts();
+                var vm = new AddDebtorViewModel(newDeptor);
+                var dlg = new AddDeptorView()
+                {
+                    DataContext = vm
+                };
+                if (dlg.ShowDialog() == true)
+                {
+                    Deptors.Add(newDeptor);
+                    CurrentDeptor = newDeptor;
+                    Index = (Deptors.Count - 1);
+                }
+            }));
+        }
+    }
+}
