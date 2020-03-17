b76f37b04e1aec58bdb9f0d5446fdedfca25b585
MortenBE <Ehlersmorten@gmail.com>
2020-03-17 10:51:42 +0100

    Styled

---
+++ GUI_A1_Gruppe20/The_debt_book/Models/Deptors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace The_Debt_Book
+{
+    public class Debtors : BindableBase
+    {
+        private string name;
+        private int totaldebt;
+        public Debtors()
+        {
+        }
+        public Debtors(string dname)
+        {
+            name = dname;
+
+            totaldebt = 0;
+        }
+        public string Debtorname
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+
+        public int InitDebt
+        {
+            get => totaldebt;
+            set => SetProperty(ref totaldebt, value);
+        }
+
+    }
+}
---
+++ GUI_A1_Gruppe20/The_debt_book/The_debt_book/ViewModel/AddDeptorViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using TheDebtBook.Models;
+
+namespace TheDebtBook
+{
+    public class AddDebtorViewModel : BindableBase
+    {
+        private Depts _deptor;
+
+        public AddDebtorViewModel(Depts deptor)
+        {
+            Deptor = deptor;
+        }
+
+        public Depts Deptor
+        {
+            get => _deptor;
+            set => SetProperty(ref _deptor, value);
+        }
+
+        private ICommand _addButton;
+
+        public ICommand AddButton
+        {
+            get => _addButton ?? (_addButton =
+                       new DelegateCommand(AddButtonExecuter, AddButtonCanExecute)
+                           .ObservesProperty(() => Deptor.Name).ObservesProperty(() => Deptor.Debt));
+        }
+
+        private void AddButtonExecuter()
+        { }
+
+        private bool AddButtonCanExecute()
+        {
+            return CheckEmpty;
+        }
+
+        public bool CheckEmpty
+        {
+            get
+            {
+                bool isValid = true;
+                if (string.IsNullOrWhiteSpace(Deptor.Name))
+                    isValid = false;
+                if (string.IsNullOrWhiteSpace(Deptor.Debt.ToString()))
+                    isValid = false;
+                return isValid;
+            }
+        }
+
+    }
+}
